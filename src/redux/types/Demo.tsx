d08908604553f7aa3d6e81566ddcf19c893d2012
Raginaakhter <raginaakhter052@gmail.com>
2026-01-06 23:52:17 +0600

    add demo file

---
+++ src/redux/types/Demo.tsx
@@ -0,0 +1,12 @@
+
+
+const Demo = () => {
+    return (
+        <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error eaque dolor maxime tempora. Sunt, iure soluta, porro quos obcaecati vero beatae,
+             laboriosam architecto blanditiis molestias earum incidunt eos laborum dolorem!
+        </div>
+    );
+};
+
+export default Demo;
