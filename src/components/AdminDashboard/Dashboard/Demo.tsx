633c255775279480176c22771b43915da82cd88f
Raginaakhter <raginaakhter052@gmail.com>
2026-01-18 21:58:43 +0600

    updated code

--- src/components/AdminDashboard/Dashboard/Demo.tsx
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -3,8 +3,7 @@
 const Demo = () => {
     return (
         <div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid 
-         Lorem ipsum dolor sit amet,Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem alias laborum sit quas nulla harum quibusdam ea, molestiae nisi. Ea distinctio illo fugiat ut excepturi esse molestiae officia, reiciendis illum.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid  estiae nisi. Ea distinctio illo fugiat ut excepturi esse molestiae officia, reiciendis illum.
         </div>
     );
 };
