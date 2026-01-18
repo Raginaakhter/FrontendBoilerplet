1f33677d92b5c961619c1dabe307da32d415626a
Raginaakhter <raginaakhter052@gmail.com>
2026-01-18 21:53:21 +0600

    updated code

--- src/components/AdminDashboard/Dashboard/Demo.tsx
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -4,7 +4,7 @@ const Demo = () => {
     return (
         <div>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid 
-            culpa neque saepe, ducimus vero? Maiores facilis impedit ducimus sit repellendus.
+           facilis impedit ducimus sit repellendus.
         </div>
     );
 };
