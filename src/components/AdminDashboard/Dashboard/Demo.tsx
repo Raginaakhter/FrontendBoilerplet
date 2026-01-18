35aba14909c427a03d7bacb64d8cbee7b8e76aaf
Raginaakhter <raginaakhter052@gmail.com>
2026-01-18 21:55:42 +0600

    delete some

--- src/components/AdminDashboard/Dashboard/Demo.tsx
+++ src/components/AdminDashboard/Dashboard/Demo.tsx
@@ -4,7 +4,7 @@ const Demo = () => {
     return (
         <div>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore nesciunt dignissimos quos cupiditate, enim accusantium reprehenderit dolor numquam aliquid 
-           facilis impedit ducimus sit repellendus.
+         
         </div>
     );
 };
