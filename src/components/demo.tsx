29a944645a923a3e421c4a2a36b6069c53cc6556
Raginaakhter <raginaakhter052@gmail.com>
2026-07-17 21:40:59 +0600

    added demo

---
+++ src/components/demo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const demo = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit ame
+            t consectetur, adipisicing elit. 
+            Asperiores soluta voluptatum perferendis consectetur. Minima nostrum facere vero provident, dolore ipsam doloribus ab omnis aperiam. Tenetur possimus odio perspiciatis nisi non.
+        </div>
+    );
+};
+
+export default demo;
