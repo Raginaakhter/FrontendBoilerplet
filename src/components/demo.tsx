de58331ebf11e0e1e68518435a230355bd2d9b7a
Raginaakhter <raginaakhter052@gmail.com>
2026-07-19 09:37:43 +0600

    incrise

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -3,7 +3,7 @@
 const demo = () => {
     return (
         <div>
-            Lorem ipsum dolor sit ame   jklnbvckgfxioreiennjjkujhui 
+            Lorem ipsum dolor sit am
             t consectetur, adipisicing elit. 
             Asperiores soluta voluptatum perferendis consectetur. Minima nostrum facere vero provident, dolore ipsam doloribus ab omnis aperiam. Tenetur possimus odio perspiciatis nisi non.
         </div>
